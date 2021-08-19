8aef62b8bfe86100f74e710bdf31a9d0d8cf3d39
tgarayua <tgarayua@gmail.com>
2021-08-19 15:22:57 -0400

    added pseudo code

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -1,10 +1,18 @@
+/**
+ * 
+ * - create an i index var that starts wtih the first index of the array (let i = 0)
+ * - loop over the rest of the arr using j as an index (let j = 1)
+ *      -if arr[i] does not equal arr[j], increment by 1 and set arr[i] to arr[j]
+ * - return i + 1;
+ */
+
 function countUniqueValues(arr){
-    if (arr.length === 0) return 0;
-    let i = 0
+    let i = 0;
+
     for (let j = 1; j < arr.length; j++) {
         if (arr[i] !== arr[j]) {
             i++;
-            arr[i] = arr[j]
+            arr[i] = arr[j];
         }
     }
     return i + 1;
