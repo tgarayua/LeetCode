9f6d94060c532edd24c523b56f3d329b7d8457b6
tgarayua <tgarayua@gmail.com>
2021-08-19 11:58:31 -0400

    finished

---
+++ countUniqueValues2.js
@@ -0,0 +1,17 @@
+const countUniqueValues = (arr) => {
+    if (arr.length === 0) return 0
+
+    let i = 0;
+
+    for (let j = 1; j < arr.length; j++) {
+        let currentNum = arr[j]
+        if (currentNum !== arr[i]) {
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    console.log(arr)
+    return i + 1;
+}
+
+console.log(countUniqueValues([1,1,1,2,2,2,2,3,4,5,5,5,5,6]))
