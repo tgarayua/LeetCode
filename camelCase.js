dc0be641be09350cfc97ef917d1d834baf008d12
tgarayua <tgarayua@gmail.com>
2021-08-20 16:26:34 -0400

    finished

---
+++ camelCase.js
@@ -0,0 +1,50 @@
+/**
+ * I Want to:
+ * - lower case each item in each array
+ * - I want to uppercase every string beyond the first
+ * - I want to concat each string into one string
+ * - I want to concat each array into one long string
+ * 
+ * 
+ * Break It Down:
+ * - create a var that takes in the result
+ * - create a for loop to loop over each arrays
+ *      - capitalize the first string in the array
+ *      - Lower case anything else !== arr[0]
+ * 
+ * 
+ */
+
+
+ const camelCase = (...args) => {
+
+    let result = ""
+    let theArgs = args[0]
+    console.log(theArgs)
+
+    for (let i = 0; i < theArgs.length;i++){
+        let arrayStr = ""
+        let thisLine = theArgs[i]
+        let endOfLine = thisLine.length -1
+        for (let j = 0; j < thisLine.length; j++){
+            let currentWord = thisLine[j].toString().toLowerCase()
+            if (j !== 0){
+                currentWord = currentWord[0].toUpperCase() + currentWord.slice(1)
+            }
+            if (j == endOfLine){
+                arrayStr += currentWord + " "
+            } else {
+                arrayStr += currentWord
+            }
+        }
+            (i === args.length - 1) ? result += arrayStr : result += `${arrayStr}`
+        }
+    return result
+}
+
+
+
+const args = [["apple", "One"], ["Apple", "TWO"], ["appLe", "thRee", "FOUR"]]
+
+let res = "appleOne appleTwo appleThreeFour"
+console.log(camelCase(args))
--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -7,44 +7,31 @@
  *      - the currentNumInSubarr is added to the maxSum var
  * - we set the tempSum to maxSum
  * - create a for loop that iterates over the arrays length
- *      - tempSum is set to the tempSum, subtracted by the first number in the subarray, plus the next number that is being added into the subarray
+ *      - tempSum is set to tempSum subtracted by the first number in the subarray, plus the next number that is being added into the subarray
  *      - maxSum is set to the larger of the two var. Use Math.max(maxSum, tempSum)
  * - return maxSum
  */
 
 const maxSubarraySum = (arr, num) => {
-    // set a var for the maxSum and for the temp max sum
     let maxSum = 0;
     let tempSum = 0;
 
-    // if the array's length is smaller than the number return null
-    if (arr.length < num) return null;
+    if (arr.length < num) return false;
 
-    // create a for loop to create the subarray 
-        // the currentNumInSubarr is added to the maxSum var
-    for(let i = 0; i < num; i++) {
-        let currentNumInSubarr = arr[i]
-        maxSum += currentNumInSubarr;
+    for (let i = 0; i < num; i++) {
+        let currentNumInSubArr = arr[i]
+        maxSum += currentNumInSubArr
     }
+    
+    tempSum = maxSum
 
-    // set tempSum = maxSum
-    tempSum = maxSum;
-
-    // for loop over the arrays length
-        // tempSum is set to the tempSum subtracted by the first number in the subarray, plus the next number that is being added into the subarray
     for (let i = num; i < arr.length; i++) {
-
-        // i = 3
-        // tempSum = 17 - (5 - 3) + 5
-        // tempSum = 17 - 2 + 5
-        // tempSum = 20
-        tempSum = tempSum - arr[i - num] + arr[i];
-
-        // maxSum is set to the larger of the two var. Use Math.max(maxSum, tempSum)
-        maxSum = Math.max(maxSum, tempSum);
+        let nextNumInSubarr = arr[i]
+        let firstNumInSubarr = arr[i - num]
+        tempSum = tempSum - firstNumInSubarr + nextNumInSubarr
+        maxSum = Math.max(maxSum, tempSum)
     }
-    // return maxSum
-    return maxSum;
+    return maxSum
 };
 //                 index = [0,1,2,3,4,5,6,7,8]
-console.log(maxSubarraySum([2,6,9,5,1,8,5,6,3],3))
+console.log(maxSubarraySum([2,6,9,2,1,8,5,6,3],3))
