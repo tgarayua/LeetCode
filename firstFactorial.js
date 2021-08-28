1e2228e5fb1564fd3ae90adde2b1ccb2e9e6fad9
tgarayua <tgarayua@gmail.com>
2021-08-27 22:56:01 -0400

    finished

--- firstFactorial.js
+++ firstFactorial.js
@@ -8,11 +8,9 @@ let num = 6;
 
 const firstFactorial = (num) => {
     let factorial = 1;
-
     for (let i = 1; i <= num; i++) {
         factorial *= i;
     }
-
     return factorial;
 };
 
---
+++ validPalindrome.js
@@ -0,0 +1,22 @@
+/* 
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+*/
+
+let str = "pap";
+
+const validPalindrome = () => {
+    let reverse = ""
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        let currentChar = str[i]
+        reverse += currentChar
+    }
+
+    if (reverse === str) {
+        return true;
+    }
+
+    return false;
+};
+
+console.log(validPalindrome(str));
