6a309cc867491cccb64639575f0633e1a359e697
tgarayua <tgarayua@gmail.com>
2021-09-12 20:54:23 -0400

    time: 7ms

--- validParentheses.js
+++ validParentheses.js
@@ -39,80 +39,45 @@ Output: true
 
 
 Break It Down
-- I want to create an parenObj
-- Populate the obj
+- I want to create an parenObj opening brackets as the key and closing brackets as the value
 - Create a stack
 - Loop over str
 - if the first char in parenObj push the value into stack
 - remove the element from the stack everytime the currentChar === closing bracket
 
  */
-
-let str = "{[()]}"
+console.time(`time`);
+let str = "()[]{}[[]]";
 
 const isValid = (str) => {
-    console.log();
-    console.log(`isValid = (str)`);
-    console.log();
-
-    console.log();
-    console.log(`Input`);
-    console.log(`- str:`, str);
-    console.log();
-    console.log(`str:`, str)
-    let string = str.toString();
-    console.log(`string:`, string)
+    if (str.length % 2 === 1) {
+        return false;
+    }
     let parenObj = {
-        '{':'}',
         '(':')',
-        '[':']'
+        '[':']',
+        '{':'}',
     };
     let stack = [];
 
-    console.log(`Global Variables (before)`);
-    console.log(`- string:`, str, ',', `parenObj:`, parenObj, ',', `stack:`, stack);
-    console.log();
-
-    console.log(`For loop Variables`)
-    // if the first char in parenObj push the value into stack
-    for (let index = 0; index < string.length; index++) {
-        let currentChar = string[index];
+    for (let index = 0; index < str.length; index++) {
+        let currentChar = str[index];
+        let lastEleOfStack = stack[stack.length - 1];
     
-        console.log(`- index:`, index,',', `string:`, string, ',', `currentChar:`, currentChar);
-    
-        for (let charKey in parenObj) {
-            if (currentChar === charKey) {
-                console.log(`If statement 1`)
-                console.log(`stack (before):`, stack);
-
-                stack.push(parenObj[charKey])
-
-                console.log(`stack (after):`, stack);
-                console.log();
-            } else if (currentChar === stack[stack.length - 1]) {
-                
-                console.log(`If statement 2`)
-                console.log(`stack (before)`, stack);
-                stack.shift();
-                console.log(`stack (after)`, stack);
-            } else if (currentChar !== charKey) {
-                return false
-            }
+        if (parenObj[currentChar]) {
+            stack.push(parenObj[currentChar]);
+        } else if (currentChar === lastEleOfStack) {
+            stack.pop();
         }
     }
-    console.log();
-
-    console.log(`Global Variables (after)`);
-    console.log(`- string:`, str, ',', `parenObj:`, parenObj, ',', `stack:`, stack);
-    console.log();
 
     if (stack.length === 0) {
         return true;
     } else if (stack.length !== 0) {
         return false;
     }
-
-    return `end!`
+    return null;
 };
 
 console.log(isValid(str));
+console.timeEnd(`time`)
